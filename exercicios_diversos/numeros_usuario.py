d990931bd0df6db5e44b8c71972a66145aa29c2b
Gabriel V Silva <47651757+GabrielVSilva25@users.noreply.github.com>
2026-09-23 22:54:30 -0300

    Relembrando a brincar com python

---
+++ exercicios_diversos/numeros_usuario.py
@@ -0,0 +1,10 @@
+numero = int(input('Informe um número: '))
+
+if numero > 0:
+    print('Número positivo.')
+
+elif numero < 0:
+    print('Número negativo.')
+
+else:
+    print('Número zero')
---
+++ exercicios_diversos/usuario_numeros.py
@@ -0,0 +1,11 @@
+
+numero = int(input('Informe um número: '))
+soma = 0
+
+while numero != 0:
+    soma += numero
+    
+    numero = int(input('Informe um número: '))
+
+print(f'A soma dos valores foi de {soma}')
+    
